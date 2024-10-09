bc7c0544eb2b6b5c45f629569a9c664594205c19
iyougeen <iyougeen@gmail.com>
2024-10-09 10:37:48 +0300

    Added index page, view, template, urls

--- backend/bot/views.py
+++ backend/bot/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Word, Player, Poll
+
 
 # Create your views here.
+def index(request):
+    words_cnt = Word.objects.count()
+    players_cnt = Player.objects.count()
+    top_five_players = Player.objects.order_by("-num_right_answers")[:5]
+    context = {"words_cnt": words_cnt, "players_cnt": players_cnt, "top_five_players": top_five_players}
+    return render(request, "bot/index.html", context)
