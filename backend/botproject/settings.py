cc210972d7f27174878230c5364b1a277eeb2adc
iyougeen <iyougeen@gmail.com>
2024-10-09 09:39:13 +0300

    Added .env file and updated settings.py with env var. Added docker volume for django project

--- backend/botproject/settings.py
+++ backend/botproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*kv*o8#th0w)o96rquzlh*a2_@1b-$^9t=po(9^4lnd7jv9q8v'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DJANGO_DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [] if os.getenv('DJANGO_ALLOWED_HOSTS') is None else os.getenv('DJANGO_ALLOWED_HOSTS').split(', ')
 
 
 # Application definition
